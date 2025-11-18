2ecf6b08bac398ba9989926fdb1f13278557b003
Adesina08 <153903110+Adesina08@users.noreply.github.com>
2025-11-18 17:16:57 +0100

    Adjust invalid submission metrics and refresh interval

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -75,7 +75,7 @@ const Index = () => {
   useEffect(() => {
     const intervalId = window.setInterval(() => {
       void handleRefresh();
-    }, 5 * 60 * 1000);
+    }, 60 * 60 * 1000);
 
     return () => {
       window.clearInterval(intervalId);
